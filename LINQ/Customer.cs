20fd1b25b7328584da0e96bccd4b4a1386fc0125
Dicheva <edicheva2@dxc.com>
2019-07-30 17:30:53 +0300

    Building a Customer & Repo classes

---
+++ LINQ/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int? CustomerTypeId { get; set; }
+
+    }
+}
---
+++ LINQ/CustomerRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public class CustomerRepository
+    {
+
+        public Customer Find(List<Customer> customerList, int customerId)
+        {
+            Customer foundCustomer = null;
+
+            //foreach (var c in customerList)
+            //{
+            //    if (c.CustomerId == customerId)
+            //    {
+            //        foundCustomer = c;
+            //        break;
+            //    }
+            //}
+
+            //var query = from c in customerList                      //Query Syntax
+            //            where c.CustomerId == customerId
+            //            select c;
+
+            //foundCustomer = query.First();
+
+            foundCustomer = customerList.FirstOrDefault(c =>    //Method Syntax
+            c.CustomerId == customerId);
+
+
+            //foundCustomer = customerList.Where(c =>
+            //c.CustomerId == customerId)
+            //    .Skip(1)                  //Chaining
+            //    .FirstOrDefault();
+
+            return foundCustomer;
+        }
+
+
+        public List<Customer> Retrieve()
+        {
+
+            List<Customer> custList = new List<Customer>
+       {
+        new Customer()
+        {
+            CustomerId = 1,
+            FirstName = "John",
+            LastName = "Smith",
+            EmailAddress = "some.email@domain.com",
+            CustomerTypeId = 1
+        },
+         new Customer()
+          {
+            CustomerId = 2,
+            FirstName = "Froddo",
+            LastName = "Baggins",
+            EmailAddress = "fb@hob.com",
+            CustomerTypeId = null
+        },
+         new Customer()
+          {
+            CustomerId = 3,
+            FirstName = "Tom",
+            LastName = "Hanks",
+            EmailAddress = "th@actor.com",
+            CustomerTypeId = 2
+        }
+        };
+            return custList;
+        }
+
+        public IEnumerable<Customer> SortByName(List<Customer> customerList)
+        {
+            return customerList.OrderBy(C => C.LastName)
+                .ThenBy(c => c.FirstName);
+        }
+
+        public IEnumerable<Customer> SortByNameInReverse(List<Customer> customerList)
+        {
+            //return customerList.OrderByDescending(c => c.LastName)
+            //    .ThenByDescending(c => c.FirstName);
+
+            return SortByName(customerList).Reverse();
+        }
+
+        public IEnumerable<Customer> SortByType(List<Customer> customerList)
+        {
+            //return customerList.OrderBy(c => c.CustomerTypeId);
+            return customerList.OrderByDescending(c => c.CustomerTypeId.HasValue)
+                .ThenBy(c => c.CustomerTypeId);
+        }
+    }
+}
